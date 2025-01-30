def37331ee1c827a2551c511e643b61840972d02
Bas de Vaan <b@sdevaan.nl>
2025-01-30 08:11:48 +0100

    publish new web version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "11972935"
+    serviceWorkerVersion: "1416447482"
   }
 });
