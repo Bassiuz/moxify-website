4a8e5a7d53744148abc944fc288ad6aa98f120f9
Bas de Vaan <b@sdevaan.nl>
2025-01-31 21:01:40 +0100

    publish new web version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3158418215"
+    serviceWorkerVersion: "2031602861"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3798dc4e6faed28219166f27e4f5cbb0",
+const RESOURCES = {"flutter_bootstrap.js": "65b3499dc0445d1ff74d433095eae696",
 "version.json": "8ca5ac52b8c70eeb73a863d783f72667",
 "index.html": "24dda9a7d7987c8197399af3ccd58da6",
 "/": "24dda9a7d7987c8197399af3ccd58da6",
