b9a29284d007cab46f3e83c843e14761b76cac5f
Bas de Vaan <b@sdevaan.nl>
2025-01-31 21:24:38 +0100

    add cname conf to publish

--- publish_to_gh_pages.sh
+++ publish_to_gh_pages.sh
@@ -1,6 +1,9 @@
-    fvm flutter build web --release --no-tree-shake-icons
-    git push origin --delete gh-pages
-    git add build/web/ -f
-    git commit -m "publish new web version"
-    git push
-    git subtree push --prefix build/web origin gh-pages
+fvm flutter build web --release --no-tree-shake-icons
+
+# Create CNAME file with custom domain
+echo "moxifymtg.com" > build/web/CNAME
+
+git add build/web/ -f
+git commit -m "publish new web version"
+git pull origin gh-pages --rebase
+git push origin $(git subtree split --prefix build/web):gh-pages --force
