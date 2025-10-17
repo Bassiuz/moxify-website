e6b95c421938eef34d379a479de854688218385f
Bas de Vaan <bas.de.vaan@medapp.nl>
2025-10-17 14:57:21 +0200

    publish new web version

--- publish_to_gh_pages.sh
+++
@@ -1,8 +0,0 @@
-fvm flutter build web --release --no-tree-shake-icons
-git push origin --delete gh-pages
-# Create CNAME file with custom domain
-echo "moxifymtg.com" > build/web/CNAME
-git add build/web/ -f
-git commit -m "publish new web version"
-git push
-git subtree push --prefix build/web origin gh-pages
