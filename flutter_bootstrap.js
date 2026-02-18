8ef32f30d1006b50d9c79f52f132644c9486bc4d
Bas de Vaan <bas.de.vaan@medapp.nl>
2026-02-18 09:58:29 +0100

    Add browser extension page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2736768396"
+    serviceWorkerVersion: "2516764721"
   }
 });
